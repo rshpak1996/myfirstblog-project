8781f5a0f7bc05e1a3a535c3ecad8bea4ed1c4aa
Roman Shpak <58186540+rshpak1996@users.noreply.github.com>
2020-11-02 02:00:25 +0200

    Add post app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    post_title = models.CharField(max_length=50)
+    post_date = models.DateTimeField(auto_now=True)
+    post_text = models.TextField()
+    post_image = models.ImageField(upload_to='post_images/')
