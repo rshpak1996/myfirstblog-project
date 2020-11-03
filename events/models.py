ddf441620ca3300927382530863265a03d23e295
Roman Shpak <58186540+rshpak1996@users.noreply.github.com>
2020-11-03 23:53:05 +0200

    mini-improvements

--- events/models.py
+++ events/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Event(models.Model):
     event_image = models.ImageField(upload_to='event_images/')
     event_text = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.event_text
